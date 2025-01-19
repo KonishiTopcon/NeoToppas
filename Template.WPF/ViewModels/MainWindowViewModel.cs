71ce52e378c1dd8482929b37c1fbf45951ae202b
konishi524 <konishi524@gmail.com>
2025-01-19 18:40:41 +0900

    サイドバー開閉実装

--- Template.WPF/ViewModels/MainWindowViewModel.cs
+++ Template.WPF/ViewModels/MainWindowViewModel.cs
@@ -13,60 +13,90 @@ namespace Template.WPF.ViewModels
         private readonly TransitionService _navigation;
         private readonly MessageService _message;
         private readonly DialogCoordinator _dialogCoordinator = new DialogCoordinator();
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public ReactivePropertySlim<string> Title { get; } = new ReactivePropertySlim<string>("NeoToppas"); //TODO:タイトル
+        public ReactivePropertySlim<string> Title { get; } = new ReactivePropertySlim<string>("NeoToppas");
         public ReactivePropertySlim<object> ActiveView { get; } = new ReactivePropertySlim<object>();
-        public ReactivePropertySlim<bool> IsProgressDialogOpen { get; } 
+        public ReactivePropertySlim<bool> IsProgressDialogOpen { get; }
         public ReactivePropertySlim<string> ProgressDialogMessage { get; }
+        public ReactivePropertySlim<string> SidebarText { get; } = new ReactivePropertySlim<string>("<");
+        public ReactivePropertySlim<double> SidebarWidth { get; } = new ReactivePropertySlim<double>(140);
         public ReactivePropertySlim<SnackbarMessageQueue> SnackbarMessages { get; }
 
         public ReactiveCommand Loaded { get; }
         public ReactiveCommand ContentRendered { get; }
         public ReactiveCommand Closed { get; }
+        public ReactiveCommand SidebarTextMouseUp { get; }
 
         public MainWindowViewModel(TransitionService navigation, MessageService messageService)
         {
-            // ContentNavigationServiceの初期設定
+            // TransitionServiceの初期化
             _navigation = navigation;
             _navigation.Initialize(view =>
             {
                 ActiveView.Value = view;
             });
 
-            // ContentMessageServiceの初期設定
+            // MessageServiceの初期化
             _message = messageService;
             _message.SetDialogContext(this);
             ProgressDialogMessage = _message.ProgressDialogMessage;
             IsProgressDialogOpen = _message.IsProgressDialogOpen;
             SnackbarMessages = _message.SnackbarMessages;
 
-            Loaded = new ReactiveCommand().WithSubscribe(() =>
-            {
-                // 呼び出された後の処理
-            });
-            ContentRendered = new ReactiveCommand().WithSubscribe(() =>
-            {
-                // 画面が表示された後の処理
-            });
-            Closed = new ReactiveCommand().WithSubscribe(() =>
-            {
-                // 画面が閉じられた後の処理
-            });
+            // コマンド初期化
+            Loaded = new ReactiveCommand().WithSubscribe(OnLoaded);
+            ContentRendered = new ReactiveCommand().WithSubscribe(OnContentRendered);
+            Closed = new ReactiveCommand().WithSubscribe(OnClosed);
+            SidebarTextMouseUp = new ReactiveCommand().WithSubscribe(OnSidebarTextMouseUp);
 
-            // 初期画面の表示
+            // 初期画面を設定
             _navigation.NavigateTo<HomeView>();
         }
 
+        private void OnLoaded()
+        {
+            // ロード時の処理をここに記述
+        }
+
+        private void OnContentRendered()
+        {
+            // 表示後の処理をここに記述
+        }
+
+        private void OnClosed()
+        {
+            // クローズ時の処理をここに記述
+        }
+
+        private void OnSidebarTextMouseUp()
+        {
+            if (SidebarText.Value == "<")
+            {
+                SidebarText.Value = ">";
+                SidebarWidth.Value = 0;
+            }
+            else
+            {
+                SidebarText.Value = "<";
+                SidebarWidth.Value = 140;
+            }
+        }
 
         public void TreeSelectedChange(string name)
         {
-            //SnackbarMessages.Value.Enqueue($"Selected Item: {name}");
-            //MessageBox.Show($"選択項目：{name}");
-            if (name == "PartsSearch"  && ActiveView.Value is not Template.WPF.Views.HomeView)
-                _navigation.NavigateTo<HomeView>();
-            else if (name == "License" && ActiveView.Value is not Template.WPF.Views.LicenseView)
-                _navigation.NavigateTo<LicenseView>();
+            switch (name)
+            {
+                case "PartsSearch":
+                    if (ActiveView.Value is not HomeView)
+                        _navigation.NavigateTo<HomeView>();
+                    break;
+                case "License":
+                    if (ActiveView.Value is not LicenseView)
+                        _navigation.NavigateTo<LicenseView>();
+                    break;
+            }
         }
     }
 }
