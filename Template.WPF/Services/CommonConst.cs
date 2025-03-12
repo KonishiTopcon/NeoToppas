5693a9f8b4cb8407419552223d323efbb7ad6665
KonishiTopcon <mkonishi@topcon.com>
2025-03-12 10:25:06 +0900

    リファクタ

--- Template.WPF/Services/CommonConst.cs
+++ Template.WPF/Services/CommonConst.cs
@@ -10,14 +10,10 @@ namespace NeoToppas.WPF.Services
     public static class CommonConst
     {
         //自動アップデート
-        //public static bool AUTOUPDATE_ENABLE = true;
-        //public static string AUTOUPDATE_DATA_FOLDER = @"\\PCD01851\SharedFolder\NeoToppasLatestVersion";
-        //public static string APP_VERSION = "1.000";
         public static string AUTOUPDATE_VERSION_FILE = "NeoToppasLatestVersionTXT.txt";
         public static string CONFIGFILE_NAME = "NeoToppas.WPF.dll.config";
 
         //ラベル印刷
-        //public static string LABEL_TEMPLATE_DIRECTORY = @"C:\Users\l057277\OneDrive - Topcon\ドキュメント\各種案件\7.TOPPAS置き換え\3.ラベルプリンタ\ブラザーラベルプリンタ\テンプレート";
         public static string LABEL_TEMPLATE_FILE = "PCBtemplate1.LBX";
 
     }
