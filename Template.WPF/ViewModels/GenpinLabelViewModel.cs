209e1a984b74c8a7cc2bbaf8d0d5224f2c86c29c
KonishiTopcon <mkonishi@topcon.com>
2025-04-02 18:49:54 +0900

    現品ラベルのガワが完成

--- Template.WPF/ViewModels/GenpinLabelViewModel.cs
+++ Template.WPF/ViewModels/GenpinLabelViewModel.cs
@@ -17,12 +17,32 @@ namespace Template.WPF.ViewModels
     {
         public event PropertyChangedEventHandler? PropertyChanged;
         public ReactivePropertySlim<string> SearchText { get; } = new ReactivePropertySlim<string>();
+        public ReactivePropertySlim<string> ItemCode { get; } = new ReactivePropertySlim<string>();
+        public ReactivePropertySlim<int> Rev { get; } = new ReactivePropertySlim<int>();
+        public ReactivePropertySlim<String> ItemName { get; } = new ReactivePropertySlim<String>();
+        public ReactivePropertySlim<DateOnly> ReceiptDate { get; } = new ReactivePropertySlim<DateOnly>();
+        public ReactivePropertySlim<String> ShelfNo { get; } = new ReactivePropertySlim<String>();
+        public ReactivePropertySlim<int> NumberOfPartsReceived { get; } = new ReactivePropertySlim<int>();
+        public ReactivePropertySlim<int> PrintCnt { get; } = new ReactivePropertySlim<int>();
+        public ReactivePropertySlim<bool> UpdateReelNum { get; } = new ReactivePropertySlim<bool>();
+        public ReactivePropertySlim<int> SplitSize { get; } = new ReactivePropertySlim<int>();
+
+
+
         public ReactiveCommand DeleteSearchTextBtn { get; }
 
         public GenpinLabelViewModel(TransitionService contentNavigation)
         {
             SearchText.Value = "";
-
+            ItemCode.Value = "T47001441A";
+            Rev.Value = 0;
+            ItemName.Value = "AD9920ARSZ";
+            ReceiptDate.Value = DateOnly.FromDateTime(DateTime.Now);//今日の日時を取得
+            ShelfNo.Value = "IC-J-06";
+            NumberOfPartsReceived.Value = 340;
+            PrintCnt.Value = 2;
+            UpdateReelNum.Value = true;
+            SplitSize.Value = 170;
             DeleteSearchTextBtn = new ReactiveCommand().WithSubscribe(async () =>
             {
                 SearchText.Value = "";
